8e07b46ab1900b9b2ab15c8e44d68014dcbc7f4c
lbod <leebodzak@hotmail.com>
2013-10-29 21:36:58 +0000

    in progress Identity.js

--- data/api/Identity.js
+++ data/api/Identity.js
@@ -1,14 +1,27 @@
+/**
+ * @module dojo/data/api/Identity
+ * @exports dojo/data/api/Identity
+ */
 define(["../../_base/declare", "./Read"], function(declare, Read){
 
 // module:
 //		dojo/data/api/Identity
-
-return declare("dojo.data.api.Identity", Read, {
+    /**
+     * @summary This is an abstract API that data provider implementations conform to.<br/>
+     *  This file defines methods signatures and intentionally leaves all the<br/>
+     *  methods unimplemented.
+     */
+return declare("dojo.data.api.Identity", Read, /** @lends module:dojo/data/api/Identity */{
 	// summary:
 	//		This is an abstract API that data provider implementations conform to.
 	//		This file defines methods signatures and intentionally leaves all the
 	//		methods unimplemented.
 
+    /**
+     * See dojo/data/api/Read.getFeatures()
+     * @returns {{dojo.data.api.Read: boolean, dojo.data.api.Identity: boolean}}
+     * @memberOf module:dojo/data/api/Identity#
+     */
 	getFeatures: function(){
 		// summary:
 		//		See dojo/data/api/Read.getFeatures()
@@ -18,6 +31,19 @@ return declare("dojo.data.api.Identity", Read, {
 		};
 	},
 
+    /**
+     * Returns a unique identifier for an item.  The return value will be<br/>
+     *  either a string or something that has a toString() method (such as,<br/>
+     *  for example, a dojox/uuid object).
+     * @param item The item from the store from which to obtain its identifier.
+     * @throws Conforming implementations may throw an exception or return null if<br/>
+     *  item is not an item.
+     *
+     * @example
+     *  var itemId = store.getIdentity(kermit);
+     *  assert(kermit === store.findByIdentity(store.getIdentity(kermit)));
+     * @memberOf module:dojo/data/api/Identity#
+     */
 	getIdentity: function(/* dojo/data/api/Item */ item){
 		// summary:
 		//		Returns a unique identifier for an item.  The return value will be
@@ -34,6 +60,23 @@ return declare("dojo.data.api.Identity", Read, {
 		throw new Error('Unimplemented API: dojo.data.api.Identity.getIdentity');
 	},
 
+    /**
+     * Returns an array of attribute names that are used to generate the identity.<br/>
+     * For most stores, this is a single attribute, but for some complex stores<br/>
+     * such as RDB backed stores that use compound (multi-attribute) identifiers<br/>
+     * it can be more than one.  If the identity is not composed of attributes<br/>
+     * on the item, it will return null.  This function is intended to identify<br/>
+     * the attributes that comprise the identity so that so that during a render<br/>
+     * of all attributes, the UI can hide the the identity information if it<br/>
+     * chooses.
+     * @param item The item from the store from which to obtain the array of public attributes that<br/>
+     *  compose the identifier, if any.
+     * @example
+     *  var itemId = store.getIdentity(kermit);
+     *  var identifiers = store.getIdentityAttributes(itemId);
+     *  assert(typeof identifiers === "array" || identifiers === null);
+     * @memberOf module:dojo/data/api/Identity#
+     */
 	getIdentityAttributes: function(/* dojo/data/api/Item */ item){
 		// summary:
 		//		Returns an array of attribute names that are used to generate the identity.
@@ -54,7 +97,23 @@ return declare("dojo.data.api.Identity", Read, {
 		throw new Error('Unimplemented API: dojo.data.api.Identity.getIdentityAttributes');
 	},
 
-
+    /**
+     * Given the identity of an item, this method returns the item that has<br/>
+     *  that identity through the onItem callback.  Conforming implementations<br/>
+     *  should return null if there is no item with the given identity.<br/>
+     *  Implementations of fetchItemByIdentity() may sometimes return an item<br/>
+     *  from a local cache and may sometimes fetch an item from a remote server,<br/>
+     * @param keywordArgs An anonymous object that defines the item to locate and callbacks to invoke when the<br/>
+     *  item has been located and load has completed.  The format of the object is as follows:<br/>
+     * @param keywordArgs An anonymous object that defines the item to locate and callbacks to invoke when the<br/>
+     *  item has been located and load has completed.  The format of the object is as follows:<br/>
+     * @param {String|Object} keywordArgs.identity
+     * @param {Function} keywordArgs.onItem
+     * @param {Function} keywordArgs.onError Function
+     * @param {Object}keywordArgs.scope
+     *
+     * @memberOf module:dojo/data/api/Identity#
+     */
 	fetchItemByIdentity: function(/* object */ keywordArgs){
 		// summary:
 		//		Given the identity of an item, this method returns the item that has
