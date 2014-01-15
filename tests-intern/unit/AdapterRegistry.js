df5c17333d93e062c750cc3a3f63ebc5c3e78573
lbod <leebodzak@hotmail.com>
2014-01-15 20:49:48 +0000

    updated as per bryans comments

--- tests-intern/unit/AdapterRegistry.js
+++ tests-intern/unit/AdapterRegistry.js
@@ -1,65 +1,64 @@
 define([
 	'intern!object',
 	'intern/chai!assert',
-	"../../AdapterRegistry",
-	'intern/dojo/has!host-browser?intern/dojo/domReady!:'
+	"../../AdapterRegistry"
 ], function (registerSuite, assert, AdapterRegistry) {
 
 	registerSuite({
 		name: 'dojo/AdapterRegistry',
 
 		'constructor': function () {
-			var taa = new AdapterRegistry();
-			assert.strictEqual(taa.pairs.length, 0);
-			assert.isFalse(taa.returnWrappers);
+			var registry = new AdapterRegistry();
+			assert.strictEqual(registry.pairs.length, 0);
+			assert.isFalse(registry.returnWrappers);
 
-			var taa = new AdapterRegistry(true);
-			assert.isTrue(taa.returnWrappers);
+			var registry = new AdapterRegistry(true);
+			assert.isTrue(registry.returnWrappers);
 		},
 		'.register': function () {
-			var taa = new AdapterRegistry();
-			taa.register("blah",
+			var registry = new AdapterRegistry();
+			registry.register("blah",
 				function(str){ return str == "blah"; },
 				function(){ return "blah"; }
 			);
-			assert.strictEqual(taa.pairs.length, 1);
-			assert.strictEqual(taa.pairs[0][0], "blah");
+			assert.strictEqual(registry.pairs.length, 1);
+			assert.strictEqual(registry.pairs[0][0], "blah");
 
-			taa.register("thinger");
-			taa.register("prepend", null, null, true, true);
-			assert.strictEqual(taa.pairs[0][0], "prepend");
-			assert.isTrue(taa.pairs[0][3]);
+			registry.register("thinger");
+			registry.register("prepend", null, null, true, true);
+			assert.strictEqual(registry.pairs[0][0], "prepend");
+			assert.isTrue(registry.pairs[0][3]);
 		},
 		'.match' : {
 			'no match': function () {
-				var taa = new AdapterRegistry();
+				var registry = new AdapterRegistry();
 				assert.throws(function() {
-					taa.match("blah");
+					registry.match("blah");
 				});
 			},
 			'returnWrappers': function () {
-				var taa = new AdapterRegistry();
-				taa.register("blah",
+				var registry = new AdapterRegistry();
+				registry.register("blah",
 					function(str){ return str == "blah"; },
 					function(){ return "blah"; }
 				);
-				assert.strictEqual(taa.match("blah"), "blah");
+				assert.strictEqual(registry.match("blah"), "blah");
 
-				taa.returnWrappers = true;
-				assert.strictEqual(taa.match("blah")(), "blah");
+				registry.returnWrappers = true;
+				assert.strictEqual(registry.match("blah")(), "blah");
 			}
 		},
 		'.unregister': function () {
-			var taa = new AdapterRegistry();
-			taa.register("blah",
+			var registry = new AdapterRegistry();
+			registry.register("blah",
 				function(str){ return str == "blah"; },
 				function(){ return "blah"; }
 			);
-			taa.register("thinger");
-			taa.register("prepend", null, null, true, true);
-			taa.unregister("prepend");
-			assert.strictEqual(taa.pairs.length, 2);
-			assert.strictEqual(taa.pairs[0][0], "blah");
+			registry.register("thinger");
+			registry.register("prepend", null, null, true, true);
+			registry.unregister("prepend");
+			assert.strictEqual(registry.pairs.length, 2);
+			assert.strictEqual(registry.pairs[0][0], "blah");
 		}
 	});
 
